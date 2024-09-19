d976324383969c0cdb046961b1b4957f26fc799a
lutfialdri <12050110415@students.uin-suska.ac.id>
2024-09-19 13:35:06 +0700

    gesture_detector exercise

--- gesture_detector_app/lib/main.dart
+++ gesture_detector_app/lib/main.dart
@@ -12,57 +12,110 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+          primarySwatch: Colors.blue),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final double boxSize = 150;
+  int numTaps = 0;
+  int numDoubleTaps = 0;
+  int numLongpress = 0;
+  double posX = 0;
+  double posY = 0;
 
-  void _incrementCounter() {
+  void center(BuildContext context) {
+    posX = (MediaQuery.of(context).size.width / 2) - boxSize / 2;
+    posY = (MediaQuery.of(context).size.height / 2) - boxSize / 2 - 30;
+
     setState(() {
-      _counter++;
+      this.posX = posX;
+      this.posY = posY;
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    if (posX == 0) {
+      center(context);
+    }
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: const Text('Gesture Detector App'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Stack(
+        children: [
+          Positioned(
+            top: posY,
+            left: posX,
+            child: GestureDetector(
+              onTap: () {
+                setState(() {
+                  numTaps++;
+                });
+              },
+              onDoubleTap: () {
+                setState(() {
+                  numDoubleTaps++;
+                });
+              },
+              onLongPress: () {
+                setState(() {
+                  numLongpress++;
+                });
+              },
+              // onVerticalDragUpdate: (details) {
+              //   print(details);
+              //   setState(() {
+              //     double delta = details.delta.dy;
+              //     posY += delta;
+              //   });
+              // },
+              // onHorizontalDragUpdate: (details) {
+              //   print(details);
+              //   setState(() {
+              //     double delta = details.delta.dx;
+              //     posX += delta;
+              //   });
+              // },
+              onPanUpdate: (details) {
+                setState(() {
+                  double deltaX = details.delta.dx;
+                  double deltaY = details.delta.dy;
+                  posX += deltaX;
+                  posY += deltaY;
+                });
+              },
+              child: Container(
+                width: boxSize,
+                height: boxSize,
+                decoration: BoxDecoration(color: Colors.red),
+                child: Column(
+                  children: [Text('PosX : $posX - PosY: $posY')],
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          ),
+        ],
+      ),
+      bottomNavigationBar: Container(
+        color: Colors.yellow,
+        padding: EdgeInsets.all(10),
+        child: Text(
+          'Taps: $numTaps - Double Taps: $numDoubleTaps - Long Press: $numLongpress',
+          style: Theme.of(context).textTheme.titleLarge,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
